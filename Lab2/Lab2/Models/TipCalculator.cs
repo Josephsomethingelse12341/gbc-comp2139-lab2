eb620c4d258ff13b48e530ffc9bec59bb0dffa0a
Josephsomethingelse12341 <Joseph.DeGois17@gmail.com>
2022-01-28 09:55:28 -0500

    Full Lab 2 - First Commit

---
+++ Lab2/Lab2/Models/TipCalculator.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Lab2.Models
+{
+    public class TipCalculator
+    {
+        [Required(ErrorMessage = "Please enter in a valid Meal Cost")]
+        [Range(0.0, 1000.0, ErrorMessage = "Cost of a meal must greater than $0 or less than or equal to $1000.0")]
+        public double? MealCost { get; set; }
+
+        public double CalculatTip(double precent)
+        {
+            if (MealCost.HasValue)
+            {
+                var tip = MealCost.Value * precent;
+                return tip;
+            }
+            return 0.0;
+        }
+
+
+    }
+}
